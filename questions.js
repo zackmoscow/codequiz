2ee0541faad523bb06533535077b9be532009716
Zack Moscow <zmoscow@gmail.com>
2019-11-07 16:25:45 -0600

    initial commit w rough html and template questions.js

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+        title: "Which of the following is NOT a valid text element?",
+        choices: ["<p>", "<h2>", "<h5>", "<h7>"],
+        answer: "<h7>"
+    },
+    {
+        title: "What is the correct method to add a new child node to an element?",
+        choices: ["pushChild", "addChild", "appendChild", "replaceChild"],
+        answer: "appendChild"
+    },
+    {
+        title: "Which of the following is NOT a geolocation object property?",
+        choices: ["coordinates", "coordinatesOptions", "position", "positionOptions"],
+        answer: "coordinatesOptions"
+    },
+  ];
